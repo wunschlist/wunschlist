eb4bc85c1a73589ca5ff342f9c3c6ea71963e406
Stefan Blos <stefan.blos@gmail.com>
2019-11-20 15:03:44 +0100

    added tests for BottomNavigationBar

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -5,26 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wunschlist/main.dart';
+import 'package:wunschlist/wunschlist_app.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('BottomNavigationBar test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(WunschlistApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our bottom navigation bar shows the 2 navigation elements
+    final RenderBox box = tester.renderObject(find.byType(BottomNavigationBar));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify bar has the correct size
+    expect(box.size.height, kBottomNavigationBarHeight);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify correct options are shown in the bar
+    expect(find.text('Wunschlist'), findsWidgets);
+    expect(find.text('Settings'), findsOneWidget);
   });
 }
