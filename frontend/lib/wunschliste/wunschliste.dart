8c2260241c829f08ab9b2a1a2e185caf7d0683a3
Stefan Blos <stefan.blos@gmail.com>
2019-11-21 17:08:37 +0100

    minor cleanup after refactoring for provider

--- frontend/lib/wunschliste/wunschliste.dart
+++ frontend/lib/wunschliste/wunschliste.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:wunschlist/model/wunsch.dart';
 import 'package:wunschlist/model/wunschlist_model.dart';
 import 'package:provider/provider.dart';
 
 class WunschlisteWidget extends StatefulWidget {
-  final WunschlistModel wunschlistModel;
-
-  WunschlisteWidget({@required this.wunschlistModel});
-
   @override
   _WunschlisteWidgetState createState() => new _WunschlisteWidgetState();
 }
 
 class _WunschlisteWidgetState extends State<WunschlisteWidget> {
-  /*List<Wunsch> wunschList = <Wunsch>[
-    Wunsch(title: "Auto"),
-    Wunsch(title: "Kopfhörer"),
-    Wunsch(title: "Handy")
-  ];
-  */
-
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: Consumer<WunschlistModel>(builder: (context, wunschlistModel, child) {
+      child:
+          Consumer<WunschlistModel>(builder: (context, wunschlistModel, child) {
         return ListView.builder(
           itemCount: wunschlistModel.wunschlist.length,
           itemBuilder: (context, index) {
