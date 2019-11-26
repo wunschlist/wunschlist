90577292b4cb40f2bff6aa6fd86deac8e2d35979
Gabriel Bauer <gabweb@gabweb.de>
2019-11-26 20:50:06 +0100

    Input validation

--- backend/src/wishes/wishe.model.ts
+++ backend/src/wishes/wishe.model.ts
@@ -1,6 +1,16 @@
-export interface Wish {
+import { IsString, Length, IsUrl, IsNumber, Min } from 'class-validator';
+import { Optional } from '@nestjs/common';
+
+export class Wish {
+    @IsString() @Length(3)
     title: string;
-    description: string;
-    link: string;
+
+    @Optional() @IsString()
+    description?: string;
+
+    @Optional() @IsUrl()
+    link?: string;
+
+    @IsNumber() @Min(0)
     createdInMs: number;
 }
